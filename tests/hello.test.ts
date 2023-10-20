5110d0451d69990423db46cea26f0041e713a4ab
Christian Miyoshi <30266081+christianmiyoshi@users.noreply.github.com>
2023-10-20 18:28:28 -0300

    Initial commit

---
+++ tests/hello.test.ts
@@ -0,0 +1,9 @@
+describe("Math functions", () => {
+  it("should multiply 5 by 3", () => {
+    expect(15).toEqual(15);
+  });
+
+  it("should add 5 by 3", () => {
+    expect(8).toEqual(8);
+  });
+});
